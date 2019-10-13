4c24e52c2fa4a31c8e9b6ca1584d9c6f3f3c0fbd
kmnicholas <kmnicholas@dmacc.edu>
2019-10-12 23:57:31 -0500

    updating for assignment 7.1

---
+++ CIS174_TestCoreApp/Controllers/InfoController.cs
@@ -0,0 +1,33 @@
+﻿/* Kyle Nicholas kmnicholas@dmacc.edu
+ * This is the controller for Assignment 7.1. 
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using CIS174_TestCoreApp.Models;
+
+namespace CIS174_TestCoreApp.Controllers
+{
+    public class InfoController : Controller
+    {
+        // The Index method opens the Info/Index view
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(InfoViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
