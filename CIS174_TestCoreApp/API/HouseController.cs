a6da41db0c907316bed7ade076995b878d0be86b
kmnicholas <kmnicholas@dmacc.edu>
2019-10-16 23:59:46 -0500

    updating for assignment 8.1

---
+++ CIS174_TestCoreApp/API/HouseController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CIS174_TestCoreApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CIS174_TestCoreApp.API
+{
+    public class HouseController : Controller
+    {
+        public IEnumerable<HouseViewModel> Houses = new HouseViewModel[]
+        {
+            new HouseViewModel
+            {
+                ID = 1,
+                Bedrooms = 4,
+                SquareFeet = 1854,
+                DateBuilt = new DateTime(1974,5,25),
+                Flooring = "Carpet"
+            },
+            new HouseViewModel
+            {
+                ID = 2,
+                Bedrooms = 3,
+                SquareFeet = 1675,
+                DateBuilt = new DateTime(2015,10,17),
+                Flooring = "Hardwood"
+            }
+        };
+
+        public List<HouseViewModel> HousesList = new List<HouseViewModel>
+        {
+            new HouseViewModel
+            {
+                ID = 1,
+                Bedrooms = 4,
+                SquareFeet = 1854,
+                DateBuilt = new DateTime(1974,5,25),
+                Flooring = "Carpet"
+            },
+            new HouseViewModel
+            {
+                ID = 2,
+                Bedrooms = 3,
+                SquareFeet = 1675,
+                DateBuilt = new DateTime(2015,10,17),
+                Flooring = "Hardwood"
+            }
+        };
+
+        [Route("api/house")]
+        [HttpGet]
+        public IActionResult ViewHouses()
+        {
+            return Ok(Houses);
+        }
+
+        [Route("api/house/{id}")]
+        [HttpGet]
+        public IActionResult ViewHouse(string id)
+        {
+            return Ok(HousesList[Int32.Parse(id) - 1]);
+        }
+    }
+}
